d8697055f87d00bfe428ef69ebe121f1cb5c2e0a
mj3c <milosjajac13@gmail.com>
2025-07-09 09:44:47 +0200

    update backend bucket name

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "incode-take-home-task"
+    bucket         = "devops-take-home-task"
     region         = "eu-central-1"
     key            = "" # Will be supplied from the command-line
     use_lockfile   = true
